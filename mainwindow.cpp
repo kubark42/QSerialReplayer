a10c9003ce8add3ff6a5a7e15a1b189979e358d5
Kenn Sebesta <kenn@eissq.com>
2014-04-12 13:58:04 -0400

    Compile error: Fix compiling on Windows.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -93,7 +93,7 @@ void MainWindow::actionConfigureSerialDeviceClicked()
 
 void MainWindow::on_newFileClicked()
 {
-	fileName = QFileDialog::QFileDialog::getSaveFileName(this, tr("Save File"), "", tr("Binary (*.ser)"));
+	fileName = QFileDialog::getSaveFileName(this, tr("Save File"), "", tr("Binary (*.ser)"));
 }
 
 void MainWindow::on_openClicked()
